9993e2ff9ac7b3dc160880cc60572234f6df6f16
allko <semerenko.vladim@gmail.com>
2026-06-15 08:27:42 +0000

    change QR format for Ukr banks

--- app/pay/[id]/PublicPaymentClient.tsx
+++ app/pay/[id]/PublicPaymentClient.tsx
@@ -69,6 +69,8 @@ export default function PublicPaymentClient({ data }: { data: PaymentData }) {
             cleanPurpose,
             "",
         ].join("\n");
+
+        setUrl(qrValue)
     }
 
     const handleCopy = (text: string, fieldName: string) => {
@@ -218,7 +220,7 @@ export default function PublicPaymentClient({ data }: { data: PaymentData }) {
 
                         <div className="bg-white p-3 rounded-2xl inline-block shadow-xl border border-white/10 my-2">
                             <img
-                                src={url ? url : `https://api.qrserver.com/v1/create-qr-code/?size=180x180&data=${encodeURIComponent(qrValue)}`}
+                                src={isUa ? url : `https://api.qrserver.com/v1/create-qr-code/?size=180x180&data=${encodeURIComponent(url)}`}
                                 alt="Payment QR Code"
                                 className="w-40 h-40 md:w-44 md:h-44"
                             />
